dd9d7fd0a989506bfcb6e47040874da4ba551ded
Deepanshu Kandpal <dkandpalz@gmail.com>
2018-05-11 12:57:03 +0530

    today's work (11 may)

--- Golang/chindi/swtich.go
+++ Golang/chindi/swtich.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
 
-func main(){
-  i :=1
-  
+func main() {
+	i := 2
+	fmt.Println("write i as ,", i)
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's a weekend")
+	default:
+		fmt.Println("It's a Weekday")
+	}
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("it's Before noon")
+	default:
+		fmt.Println("it's after noon ")
+	}
 }
+  
