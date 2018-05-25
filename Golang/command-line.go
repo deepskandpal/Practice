5ae2acfb6d24c74b07ca6d4592a74812f1b8c6e4
Deepanshu Kandpal <dkandpalz@gmail.com>
2018-05-25 12:10:55 +0530

    swift and golang

--- Golang/command-line.go
+++ Golang/command-line.go
@@ -1,15 +1,21 @@
+//echo functionality of the terminal build using go
+
 package main //defining the package to which the source code belongs to
 
 import (
-	"fmt" //importing formant package for basic IO functions
-	"os"  // importing OS package for os.Args variable to take command line agruements
+	"fmt"     //importing formant package for basic IO functions
+	"os"      // importing OS package for os.Args variable to take command line agruements
+	"strings" //for the Join function
 )
 
 func main() {
-	var s, sep string                   // defining 2 variables s ande sep as string both if not initialized explicitly will be by default initialized to empty string
-	for i := 1; i < len(os.Args); i++ { //run the for loop till the time the user is entereing the arguements
-		s += sep + os.Args[i] //concatenate sep and the os argument together in s
-		sep = " "             //sep is a space
+	s, sep := "", ""                  // defining 2 variables s ande sep as string both if not initialized explicitly will be by default initialized to empty string
+	for _, arg := range os.Args[1:] { //blank identifier when syntax requires variable but the logic doesn't
+		s += sep + arg //concatenate sep and the os argument together in s
+		sep = " "      //sep is a space
 	}
-	fmt.Println(s) //print s
+
+	fmt.Println(os.Args[0])
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	//fmt.Println(os.Args[1:])
 }
--- Golang/image.go
+++ Golang/image.go
@@ -4,44 +4,57 @@ import (
 	"image"
 	"image/color"
 	"image/gif"
+	"io"
 	"math"
 	"math/rand"
 	"os"
-	"io"
 )
 
-var palette =[]color.Color{color.White,color.Black} //composite literals (slice)
+var palette = []color.Color{color.White, color.Black} //composite literals (slice)
 const (
-	whiteIndex =0
-	blackIndex =0
+	whiteIndex = 0
+	blackIndex = 0
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	//!+main
 	lissajous(os.Stdout)
 }
 
-func lissajous (out io.Writer){
+func lissajous(out io.Writer) {
 	const (
-		cycles =5
-		res    =0.001
-		size   =100
-		nframes=64
-		delay  =8
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
 	)
-	freq :=rand.Float64()*3.0 
-	anim :=gif.GIF{LoopCount: nframes} //composite literals (structure)
-	phase:=0.0 
-	for i:=0;i<nframes; i++ {
-		rect :=image.Rect(0,0,2*size+1,2*size+1)
-		img  :=image.NewPaletted(rect,palette)
-		for t:=0.0; t<cycles*2*math.Pi;t +=res {
-			x:=math.Sin(t)
-			y:=math.Sin(t*freq+phase)
-			img.SetColorIndex(size+int(x*size+0.5),size+int(y*size+0.5),blackIndex)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes} //composite literals (structure)
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 		}
-		phase+=0.1
-		anim.Delay = append(anim.Delay,delay)
-		anim.Image = append(anim.Image,img)
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out,&anim)
+	gif.EncodeAll(out, &anim)
 }
