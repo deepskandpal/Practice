f7d33d5c396bccb5bb9fab77a3b62f89d0b0934a
Deepanshu Kandpal <dkandpalz@gmail.com>
2018-06-06 13:11:52 +0530

    updates on functions

--- swift/collections.swift
+++ swift/collections.swift
@@ -262,3 +262,79 @@ func fetchPlayerScores (closure:ArrayClosure) {
   let scores = [934,222,101]
   closure(scores)
 }
+func saluteHunter () {
+    print("hello there intrepid hunter")
+}
+
+saluteHunter()
+
+func findNearestHunter() -> String{
+    return "Argus"
+    
+}
+
+var nearMe = findNearestHunter()
+
+func equippedItems (armour : String) -> Bool {
+    print("\(armour) successfully equipped")
+    return true
+}
+
+equippedItems(armour: "Deamon vest")
+
+
+func requestItemTrade (myItem : String) -> (yourItem: String, value :Int){
+    print("can I trade my \(myItem)?")
+    return ("Sacred Sheild " , 300)
+}
+
+let tradeItem = requestItemTrade(myItem: "Old Hat")
+
+print("recieved your item \(tradeItem.yourItem), valued at \(tradeItem.value)\n")
+
+func requestTrade (item : String)-> String? {
+    let returnItem = "garbage sword"
+    return returnItem
+}
+if let item = requestTrade(item: "Spike Boots"){
+    print("\(item) recieved")
+    
+}
+else {
+    print("trade fell through\n")
+}
+
+func setupArenaMatch (level:String = "fire Marshes" , noOfOponents:Int = 2) {
+    print("your arena match will take place in \(level) between \(noOfOponents) players")
+}
+
+setupArenaMatch()
+setupArenaMatch(level : "Poison Flats" ,noOfOponents: 5)
+
+func attack () {
+    print("attackinh")
+    
+}
+
+func attack (damage :Int) {
+    print("attacking for \(damage) damage")
+}
+
+func attack (damage:Double , weapon: String) -> Bool {
+    print("\(damage) done to enemy with \(weapon)")
+    return true
+}
+
+attack()
+attack(damage: 35)
+attack(damage: 45, weapon:"Hammer")
+
+func computeBonusDamage (damage:Int) -> Int {
+    return damage+1
+}
+
+func dealDamage(baseDamage:Int,computeFunc:(Int)->Int) {
+    let bonus = computeFunc(baseDamage)
+    print("base damage \(baseDamage)\n \(bonus) \n\n  \(bonus + baseDamage) total damage dealt to the enemy")
+}
+
