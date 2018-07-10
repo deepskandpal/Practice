a0cb9f9cf170e9b8d15aace6496e08114eafa9d2
Deepanshu Kandpal <dkandpalz@gmail.com>
2018-07-10 16:27:45 +0530

    addition of title and backbarbutton

--- basicCell/basicCell/ViewController.swift
+++ basicCell/basicCell/ViewController.swift
@@ -9,16 +9,22 @@
 import UIKit
 import SDWebImage
 
+private let refreshControl =  UIRefreshControl()
+
 class ViewController: UITableViewController {
-    
     var newsItem = [[String:String]]()
+    
+    if #available(iOS 10.0, *) {
+    .refreshControl = refreshControl
+    } else {
+    addSubview(refreshControl)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         let nib = UINib(nibName: "TableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "tableCell")
         let url:String
-        
-        self.navigationController?.title = "NY TIMES"
+        navigationItem.title = "NY Times Most Popular"
         url = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=1bdd7e3e3c9b4e2aba6689c50f43b6b0"
         if let url = URL(string: url) {
             if let data = try?String (contentsOf: url){
@@ -58,12 +64,11 @@ extension ViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let newsItems = newsItem[indexPath.row]
         let cell:TableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "tableCell") as! TableViewCell
-        let url = URL(string: "https://static01.nyt.com/images/2018/07/02/world/02mexico-top/02mexico-top-thumbStandard.jpg")
+      //  let url = URL(string: "https://static01.nyt.com/images/2018/07/02/world/02mexico-top/02mexico-top-thumbStandard.jpg")
         cell.header.text = newsItems["title"]
         cell.abstract.text = newsItems["abstract"]
         cell.date.text = newsItems["date"]
         cell.byLine.text = newsItems["byline"]
-        cell.photo.sd_setImage(with: url, placeholderImage:nil)
         return cell
     }
     
