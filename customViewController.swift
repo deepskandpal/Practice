c5dddffdd1c2eb297da5b44a18a392b74b91ad29
Deepanshu Kandpal <dkandpalz@gmail.com>
2018-08-31 18:47:31 +0530

    some changes

--- customViewController.swift
+++ customViewController.swift
@@ -7,29 +7,42 @@
 //
 
 import UIKit
+import GPUImage2
 
-class customViewController: UIViewController {
-
+class ViewController: UIViewController {
+    "https://s3.ap-south-1.amazonaws.com/jep-content/HelloJio/videos/rb/mp4/BTYQ1-Q9.mp4"
+    @IBOutlet weak var renderView: UIView!
+    var newRenderView = RenderView()
+    var movie:MovieInput!
+    var filter:MonochromeFilter!
+    var newFilter:ChromaKeyBlend!
+    var sourceImage:PictureInput!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string:"https://jep-asset.akamaized.net/HelloJio/configs/rb/rb_bg.jpg")
+        let bundleURL = Bundle.main.resourceURL!
+        let movieURL = URL(string:"sample.mp4", relativeTo:bundleURL)!
 
-        // Do any additional setup after loading the view.
+        do {
+            movie = try MovieInput(url:movieURL, playAtActualSpeed:true)
+            newFilter = ChromaKeyBlend()
+            movie --> newFilter --> newRenderView
+            if let data = try? Data(contentsOf: url!)
+            {
+                let backgroundImage: UIImage = UIImage(data: data)!
+                sourceImage = PictureInput(image: backgroundImage)
+                sourceImage.addTarget(newFilter)
+                sourceImage.processImage()
+            }
+            movie.runBenchmark = true
+            movie.start()
+            renderView.addSubview(newRenderView)
+            newRenderView.frame = view.bounds
+        } catch {
+            print("Couldn't process movie with error: \(error)")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+
